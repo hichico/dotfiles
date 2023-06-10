fae7c76caf8aab112ef67bda8692a459dad2717b
Shik0 <olachico@icloud.com>
2023-06-10 14:32:54 +0100

    start flake from zero

--- flake.nix
+++ flake.nix
@@ -19,50 +19,6 @@
   };
 
   outputs = inputs: {
-    darwinConfigurations.mbp2016 = inputs.darwin.lib.darwinSystem {
-      system = "aarch64-darwin";
-        pkgs = import inputs.nixpkgs-stable {
-          system = "aarch64-darwin";
-        };
-      };
-      nixosModules = [
-        ({pkgs, ...}: {
-          # here goes the darwin preferences and configuration
-          programs.fish.enable = true;
-          environment.shells = [ pkgs.bash pkgs.fish ];
-          environment.loginShell = [ pkgs.fish ];
-          nix.extraOptions = ''experimental-features = nix-command flakes'';
-          systemPackages = [ pkgs.coreutils ];
-          system.keyboard.enableKeyMapping = true;
-          services.nix-daemon.enable = true;
-          system.defaults.finder.AppleShowAllExtensions = true;
-          system.defaults.dock.autohide = true;
-          system.defaults.NSGlobalDomain.InitialKeyRepeat = 14;
-          system.defaults.NSGlobalDomain.KeyRepeat = 1;
-        })
-        (inputs.home-manager.darwinModules.home-manager {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPkgs = true;
-            users.shik0.imports = [
-              ({pkgs, ...}: {
-                # Specify my home manager configs
-                home.packages = [ pkgs.ripgrep pkgs.fd pkgs.curl pkgs.less ];
-                programs.bat.enable = true;
-                programs.fzf.enable = true;
-                programs.exa.enable = true;
-                programs.git.enable = true;
-                programs.fish.enable = true;
-                programs.starship.enable = true;
-                programs.alacritty = {
-                  enable = true;
-                  settings.font.size = 16;
-                };
-              })  
-            ];
-          };
-        })
-      ];
+
   };
-    
 }
